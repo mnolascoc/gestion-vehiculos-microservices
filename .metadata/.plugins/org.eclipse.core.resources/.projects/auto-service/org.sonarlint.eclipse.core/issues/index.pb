e209833b3ef524c3aa207eadf8ac3f642519b9df
Miguel Nolasco <mnolascoc@outlook.com>
2022-05-14 12:23:27 -0500

    Se agrega eureka service

--- .metadata/.plugins/org.eclipse.core.resources/.projects/auto-service/org.sonarlint.eclipse.core/issues/index.pb
+++ .metadata/.plugins/org.eclipse.core.resources/.projects/auto-service/org.sonarlint.eclipse.core/issues/index.pb
@@ -1,6 +1,4 @@
 
-Y
-)src/main/resources/application.properties,0\6\06f65155ec73600b9fc5c4dc2883d4bb84d8e284
 m
 =src/main/java/com/coremei/auto/repository/AutoRepository.java,5\a\5af8aa22fe8361dcb137a5f3041f90e6ffb66116
 g
--- auto-service/src/main/java/com/coremei/auto/AutoServiceApplication.java
+++ auto-service/src/main/java/com/coremei/auto/AutoServiceApplication.java
@@ -2,7 +2,9 @@ package com.coremei.auto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class AutoServiceApplication {
 
